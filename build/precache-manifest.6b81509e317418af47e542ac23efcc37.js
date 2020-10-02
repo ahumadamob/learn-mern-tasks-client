a7af53e82850a56a457d964bac1664518445d956
Mario Ahumada <ahumadamob@gmail.com>
2020-10-02 20:22:17 -0300

    Ajustando la dirección del servidor

---
+++ build/precache-manifest.6b81509e317418af47e542ac23efcc37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bfe56b0c43ca09a8690aff3cfa16ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3af44565abf56a2cf93",
+    "url": "/static/css/main.733a94b3.chunk.css"
+  },
+  {
+    "revision": "b0e155a7aa69d3c78317",
+    "url": "/static/js/2.7b73e854.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b73e854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3af44565abf56a2cf93",
+    "url": "/static/js/main.89485c9f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
